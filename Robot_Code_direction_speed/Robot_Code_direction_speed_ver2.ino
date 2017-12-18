1606e64e0fda7316fbb7cd504ba1a0f37fc88c2e
damiandziedzic <34611034+damiandziedzic@users.noreply.github.com>
2017-12-18 18:29:59 -0500

    Add files via upload

---
+++ Robot_Code_direction_speed/Robot_Code_direction_speed_ver2.ino
@@ -0,0 +1,124 @@
+// This code receives the data sent from sensors, scales it, and applies PWM to motors.
+// NOTE: for this code to work, it assumes that the wheels move in opposite direction.
+// If robot is flipped upside down, the wires from the driver boards to the wheels should be:
+// BLACK | RED | BLACK | RED (in order from left to right)
+
+#include <SPI.h>
+#include "RF24.h"
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 0;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7, 8);
+/**********************************************************/
+
+byte addresses[][6] = {"ABCD", "EFGH"};
+
+
+void setup() {
+  Serial.begin(115200);
+
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if (radioNumber) {
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1, addresses[0]);
+  } else {
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1, addresses[1]);
+  }
+
+  // Start the radio listening for data
+  radio.startListening();
+
+
+  // Pin Setup
+  DDRB |= (1 << PB1) | (1 << PB2);
+
+  // PWM Setup
+  // CW   -> 1520us - 2000us // For each mode this is the time where the PWM signal is HIGH, so duty cycle
+  // CCW  -> 1000us - 1480us
+  // Stop -> 1480us - 1520us
+  // Max time high needed is 2000us -> 500 Hz
+  // If use CLK/64 for 9-bit timer on TIMER1, then top is 511 and after 511 ticks you have 2044us (WGM1: 0110)
+  // Our max time high is 2000us which is 97.85% duty cycle of 2044us time total
+  TCCR1A = 0b10100010; // Fast PWM, non-inverting, WGM1[1:0]
+  TCCR1B = 0b00001011; // WGM1[3:2], CLK/64 pre-scaler
+  
+ // OCR1A/B * 4 = corresponding time HIGH in usec
+ // Set to complete stop in setup
+  OCR1A = 375;
+  OCR1B = 375;
+}
+
+// Recieved data array
+unsigned long data[2];
+// data[0] -> Speed Increment from the base value below
+// data[1] -> Direction
+
+// Base values, 375 is full stop
+unsigned long base_val = 375;
+
+void loop() {
+
+  if ( radio.available()) {
+
+    while (radio.available()) {                                   // While there is data ready
+      radio.read( &data, sizeof(data) );                          // Get the payload
+    }
+
+    char response[3] = "ACK";                                     // Acknowledge response for the sensor ouput
+    radio.stopListening();                                        // First, stop listening so we can talk
+    radio.write( &response, sizeof(unsigned long) );              // Send the final one back.
+
+    radio.startListening();                                       // Now, resume listening so we catch the next packets.
+
+    unsigned long change = data[1];                               // New direction 
+    long leftOrRight= 25-change;                                  // If negative left wheel faster, if postive right wheel faster 
+    unsigned long turnValue = abs(leftOrRight);                   // change scale to effect speed 
+    unsigned long speed_mapped = map(data[0], 0, 30, 0, 125);     // USE THIS FOR MAGNETIC SENSOR
+
+    //OCR1A --> Right wheel NEEDS TO BE SUBTRACTED FROM BASE TO SPEED UP -> do no exceed bounds of min: 250, max: 375
+    //OCR1B --> Left wheel NEEDS TO BE ADDED TO BASE TO SPEED UP -> do not exceed bounds of min: 375, max: 500
+
+    if (speed_mapped == 0){ // Make sure to stop if speed_mapped is zero
+      OCR1A = base_val;
+      OCR1B = base_val;
+    }
+    else{ // Otherwise scale values
+      if (leftOrRight > 0) {  // right wheel faster so turn left
+        OCR1A = constrain(base_val - speed_mapped - turnValue, 250, 375); // right wheel goes faster
+        OCR1B = constrain(base_val + speed_mapped - turnValue, 375, 500); // left wheel goes slower
+//        OCR1B = constrain(base_val + speed_mapped, 375, 500);
+      }
+      else if (leftOrRight < 0) { //right faster 
+        OCR1A = constrain(base_val - speed_mapped + turnValue, 250, 375); // right wheel goes slower
+//        OCR1A = constrain(base_val - speed_mapped, 250, 375); 
+        OCR1B = constrain(base_val + speed_mapped + turnValue, 375, 500); // left wheel goes faster
+      }
+      else {  // same speed 
+        OCR1A = base_val - speed_mapped;
+        OCR1B = base_val + speed_mapped;
+      }
+    }
+    
+    // Try the following tunings to improve driving performance.
+    // NOTE: Check if there is better perfomance if only one wheel is sped up instead of also slowing down the opposite wheel.
+    // NOTE: Maybe try different scalings of turnValue.
+
+    Serial.print(F("Got Sensor Output1 "));
+    Serial.print(data[0]);
+    Serial.print(", ");
+    Serial.print(F("Got Sensor Output2 "));
+    Serial.print(data[1]);
+    Serial.print(F(", Sent response "));
+    Serial.println(response);
+  }
+}
---
+++ Sensor_Output_direction_speed/Sensor_Output_direction_speed_ver2.ino
@@ -0,0 +1,163 @@
+// Sensor uses Incremental Quadrature AB output
+// Output A is counted every falling edge, and Output B is 90 degrees out of phase of A
+// So at every falling edge of A, look at B to see if it is high or low. This represents the 
+// direction in which the magnetic strip is moving.
+
+// AS5304 sensor resolution is 25um. So one period, or every falling edge represents 0.1mm in distance or one increment/decrement of count.
+
+// Rotary encoder does not show distance resolution in datasheet. But contains 24-pulse encoder with detents.
+// Can still use "incrementing count method" in the ISR. But there is no distance index to use. Will need to scale
+// rotary sensor differently from AS5304 to get OCR1A/B values.
+// NOTE: Rotary encoder (PEC11 Series) was not very stable compared to magnetic sensor
+
+#define distance_index 0.1          // Distance in mm for every interrupt event, use if want to implement distance instead of count number (magnetic sensor)
+volatile unsigned long count1 = 0;  // Number of distance counts for sensor 1, also the speed increment
+volatile unsigned long count2 = 25; // Number of distance counts for sensor 2, alse the direction value. Start at 25 for halfway point to be straight.
+
+#include <SPI.h>
+#include "RF24.h"
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 1;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+/**********************************************************/
+
+byte addresses[][6] = {"ABCD","EFGH"};
+
+
+void setup() {
+  Serial.begin(115200);
+    
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+ // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  
+  // Start the radio listening for data
+  radio.startListening();
+
+  // Set up pins
+  DDRB &= 0b11111001;                            // Make sure direction is input for desired pins of port B and D
+  DDRD &= 0b10110011;                            
+  PORTD |= (1 << PD2) | (1 << PD3) | (1 << PD6); // Pullup resistors for pins using sensors
+  PORTB |= (1 << PB1);
+
+
+  // Setup external interrupts
+  EICRA |= 0b00001010;                // Trigger interrupt for both sensors at falling edge
+  EIMSK |= (1 << INT1) | (1 << INT0); // Enable both external interrupt pins
+  // EIFR is the flag register for both external interrupts
+
+}
+
+// Create array of the two values
+unsigned long data[2];
+
+void loop() {
+    data[0] = count1;   // Assign sensor values to data array
+    data[1] = count2;
+  
+    radio.stopListening();                                          // First, stop listening so we can talk.
+    
+    Serial.println(F("Now sending"));
+     if (!radio.write( &data, sizeof(data) )){
+       Serial.println(F("failed"));
+     }
+
+    radio.startListening();                                    // Now, continue listening
+    
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout ){                                             // Describe the results
+        Serial.println(F("Failed, response timed out."));
+    }else{
+        char got_response[3];                                   // Grab the response, compare, and send to debugging spew
+        radio.read( &got_response, sizeof(got_response) );
+        
+        // Spew it
+        Serial.print(F("Sent Sensor Output "));
+        Serial.print(count1);
+        Serial.print(", ");
+        Serial.print(count2);
+        Serial.print(F(", Got response "));
+        Serial.println(got_response);
+    }
+    
+//  For debugging purposes   
+  Serial.print(data[0]);
+  Serial.print(" / ");
+  Serial.println(data[1]);
+}
+
+// Interrupt for speed sensor
+ISR(INT0_vect){
+ static unsigned long last_interrupt_time0 = 0;
+ unsigned long interrupt_time0 = millis();
+ if (interrupt_time0 - last_interrupt_time0 > 50){ // Debouncing, which waits 100ms to change value
+  if(PINB & (1<<PB1)){                              // If sensor 1 direction HIGH
+    if (count1 != 30)                               // Do not go above 12 for rotary, 30 for magnetic
+     count1++;                                      // Increment sensor 1 counter
+  }                                                 
+  else if((PINB & (1<<PB1)) == 0){                  // If sensor 1 direction LOW
+    if(count1 != 0)                                 // Do no go below 0
+     count1--;                                      // Decrement sensor 1 counter
+  }                                              
+ last_interrupt_time0 = interrupt_time0;
+ }
+}
+
+
+// Had to use a different pin to read direction pin to not interfere with wireless module
+// Interrupt for direction sensor
+ISR(INT1_vect){
+ static unsigned long last_interrupt_time1 = 0;
+ unsigned long interrupt_time1 = millis();
+ if (interrupt_time1 - last_interrupt_time1 > 50){
+  if(PIND & (1<<PD6)){                              // If sensor 2 direction HIGH
+    if (count2 != 50)                               // Do not go above 50
+     count2++; 
+  }                                                 // Increment sensor 2 counter
+  else if((PIND & (1<<PD6)) == 0){                  // If sensor 2 direction LOW
+    if(count2 != 0)                                 // Do not go below 0
+     count2--;                                      // Decrement sensor 2 counter
+     }      
+ last_interrupt_time1 = interrupt_time1;
+ }
+}
+
+
+// DOES NOT WORK, for some reason cannot use the SS pin when using wireless module, even though its SS is on a different pin
+//ISR(INT1_vect){
+// static unsigned long last_interrupt_time1 = 0;
+// unsigned long interrupt_time1 = millis();
+// if (interrupt_time1 - last_interrupt_time1 > 100){
+//  if(PINB & (1<<PB2)){                            // If sensor 2 direction HIGH
+//     count2++; }                                  // Increment sensor 2 counter
+//  else if((PINB & (1<<PB2)) == 0){                // If sensor 2 direction LOW
+//    if(count2 != 0)
+//     count2--;                                    // Decrement sensor 1 counter
+//     }      
+// last_interrupt_time1 = interrupt_time1;
+// }
+//}
